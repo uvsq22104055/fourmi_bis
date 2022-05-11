7069133fe1cc351aebf20c9bd158f868b8dd70f7
Prénom NOM <prenom.nom@ens.uvsq.fr>
2022-05-11 14:45:43 +0200

    next

--- programme.py
+++ programme.py
@@ -9,12 +9,17 @@ def play():
 def pause():
     canvas.itemconfigure()
 
+def next():
+    canvas.itemconfigure()
+
 racine = tk.Tk()
 canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
 bouton_play = tk.Button(racine, text="play", command=play)
 bouton_pause =tk.Button(racine, text="pause", command=pause)
+bouton_next= tk.Button(racine, text='pause', command=pause)
 
 canvas.grid()
 bouton_play.grid(row=0, column=1)
 bouton_pause.grid(row=1, column=1)
+bouton_next.grid(row=2, column=1)
 racine.mainloop()
